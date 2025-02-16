18d3da3aeb8ac29e4a6e70b6950468adcf62fb53
Ajay Khan <Ajaykhan6433@gmail.com>
2025-02-16 16:56:10 +0530

    Added navbar and footer

---
+++ components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+        <footer className='w-full h-[80px] absolute bottom-0  text-center p-6 gap-4 shadow-[0px_-4px_6px_rgba(0,0,0,0.1)]'>
+            <p>Created By Ajay6433</p>
+            <p>Blog App &copy; 2025</p>
+        </footer>
+  )
+}
+
+export default Footer
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,11 +3,11 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='flex justify-between w-screen h-14 bg-slate-500'>
+    <div className='w-full h-[80px] flex justify-between items-center p-6 gap-4 shadow-md'>
         <div>
-            <h1>Blog-App</h1>
+            <Link href={"/"} className='text-4xl font-bold'>Blog App</Link>
         </div>
-        <div className='bg-red-800'>
+        <div className=' w-100 flex justify-between items-center p-3 gap-4'>
             <Link href={"/"}>Home</Link>
             <Link href={"#"}>Create Blogs</Link>
             <Link href={"#"}>View Blogs</Link>
